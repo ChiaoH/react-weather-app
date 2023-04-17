1c012530b6b50fb264caa614a2c7b3add42618f9
ChiaoH <118980040+ChiaoH@users.noreply.github.com>
2023-04-17 17:32:32 +0200

    Added Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="btn btn-warning">Hi!</div>
+        Weather App
+        <footer>
+          This app was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/wu-chiao-huang-642a07255/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Wu-chiao
+          </a>
+          , and is{" "}
+          <a
+            href="https://github.com/ChiaoH/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+          .
+        </footer>
+      </div>
+    </div>
+  );
 }
