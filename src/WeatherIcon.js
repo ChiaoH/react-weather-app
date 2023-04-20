923f91c6a1bfad0f6b865b6a635bb58ce894d2e2
ChiaoH <118980040+ChiaoH@users.noreply.github.com>
2023-04-20 11:27:53 +0200

    Deleted React that's never used. Tried to puch again.

---
+++ src/WeatherIcon.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function WeatherIcon(props) {
+  return (
+    <div className="weatherIcon">
+      <img
+        src="https://raw.githubusercontent.com/divyanshu013/react-animated-weather/HEAD/react-animated-weather.gif"
+        alt={props.alt}
+      ></img>
+    </div>
+  );
+}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -17,8 +18,13 @@ export default function WeatherInfo(props) {
       </div>
       <div className="row">
         <div className="col-6">
-          <div className="clear-fix">
-            <img src={props.data.icon} alt={props.data.description}></img>
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon
+                code={props.data.icon}
+                alt={props.data.description}
+              />
+            </div>
             <span className="temperature">
               {Math.round(props.data.temperature)}
             </span>
