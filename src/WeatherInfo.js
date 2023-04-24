48e806438c38ec6f109421aeaac620661c020047
ChiaoH <118980040+ChiaoH@users.noreply.github.com>
2023-04-24 11:25:05 +0200

    Added WeatherForecast.js and its CSS file.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -21,7 +21,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div>
-              <WeatherIcon code={props.data.icon} />
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
             <CurrentTemp celsius={props.data.temperature} />
           </div>
